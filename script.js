a709c63549f73bd8ddcdf1bad21010ad8aa6b952
kibiraG <kibiragjunior@gmail.com>
2024-01-16 13:01:09 +0300

    update

--- script.js
+++ script.js
@@ -16,17 +16,56 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+// object are indicated by curly braces {}
+//object properties are written as(name of the property) key: value (value that property holds) pairs
+const locations = [{
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"] //surround the property name with quotes because it has more than one word
+}]; 
 
 // initialize button
-button1.onclick = goStore;
-button2.onClick = goCave;
-button3.onClick = fightDragon;
+/*The onclick event generally occurs when the user clicks an element.It allows the programmer to
+execute a javascript's function when an element gets clicked */
+button1.onclick = goStore; // When you click this button you go to store
+button2.onClick = goCave; // When you click this button you go to cave
+button3.onClick = fightDragon;// when you click this button you fight dragon
 
 
+function update(location) {
+
+}
+
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+
+    button1.onClick = goStore;
+    button2.onClick = goCave;
+    button3.onClick = fightDragon;
+
+    text.innertext = "You are in the town square. You see a sign that says Store.";
+
+    
+}
+
+
+// innerText returns all text contained by an element and all its child elements.
+// It controls the text that appears in an HTML element.
+// innerHtml on the other hand returns all text, including html tags, that is contained by an element. 
+
+//When you click "Go to store" button it changes the buttons and text to the following
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
+
+    button1.onClick = buyHealth;
+    button2.onClick = buyWeapon;
+    button3.onClick = goTown;
+
+    // Escape sequence for double quote is \"
+    text.innerText = "You enter the \"store\"";
 
 }
 
@@ -37,4 +76,13 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
 
