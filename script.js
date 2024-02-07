9c561f89b4af53ba11b69a8ec13d30e3542a99b2
kibiraG <kibiragjunior@gmail.com>
2024-02-07 14:08:24 +0300

    update

--- script.js
+++ script.js
@@ -99,6 +99,11 @@ const locations = [
     "button text": ["REPLAY", "REPLAY", "REPLAY"],
     "button functions": [restart, restart, restart],
     text: "You die ☠️."
+}, {
+    name: "win",
+    "button text": ["REPLAY", "REPLAY", "REPLAY"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! 🎉."
 }
 ]; 
 
@@ -281,6 +286,11 @@ function defeatMonster() {
 
 function lose() {
     update(locations[5]);
+
+}
+
+function winGame() {
+    update(locations[6]);
 
 }
 
